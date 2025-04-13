6b459386c080bd77e5fe8db54d965e25d9e77e7e
Wilber Barquero <wilbernoel@hotmail.com>
2025-04-12 19:07:48 -0500

    adding sample data to h2 DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,15 @@
+DROP TABLE IF EXISTS products;
+
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     quantity INT NOT NULL
 );
+
+-- Sample data
+INSERT INTO products (name, quantity) VALUES 
+('Laptop', 10),
+('Monitor', 15),
+('Keyboard', 30),
+('Mouse', 50),
+('USB Drive', 100);
